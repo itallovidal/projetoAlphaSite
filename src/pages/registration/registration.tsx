e22606fdf42eee9a7800d549ef572916d403dba4
italloVidal <itallovidalll@gmail.com>
2023-10-08 15:44:36 -0300

    rotas

--- src/pages/registration/registration.tsx
+++ src/pages/registration/registration.tsx
@@ -22,7 +22,7 @@ function Registration() {
             <Styles.Content>
                 <Styles.FormWrapper>
                     <Styles.Header>
-                        <h1>{JSON.stringify(politic)}</h1>
+                        <h1>Cadastramento</h1>
                         <h3>Precisaremos de algumas informações.</h3>
                     </Styles.Header>
 
